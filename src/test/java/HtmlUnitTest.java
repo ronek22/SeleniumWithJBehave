3278ec94b0c0900773e21a3bd069b9f756614b3c
Jakub Ronkiewicz <jronek3010@gmail.com>
2018-06-03 15:37:31 +0200

    HtmlUnit with search book tests

---
+++ src/test/java/HtmlUnitTest.java
@@ -0,0 +1,52 @@
+import PageObject.PageObjectBook;
+import PageObject.PageObjectLogin;
+import io.github.bonigarcia.SeleniumExtension;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(SeleniumExtension.class)
+public class HtmlUnitTest
+{
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    public HtmlUnitTest() {
+        this.driver = new HtmlUnitDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @BeforeEach
+    public void setDefaultPage() throws Exception {
+        PageObjectLogin loginPage = PageFactory.initElements(driver, PageObjectLogin.class);
+        loginPage.login("admin@admin.com", "admin1");
+        loginPage.bookPage();
+    }
+
+    @Test
+    public void searchTestForJanPhrase(){
+        PageObjectBook books = PageFactory.initElements(driver, PageObjectBook.class);
+        books.searchBooks("Jan");
+        assertEquals(2, books.sizeOfTable());
+    }
+
+    @Test
+    public void searchTestFor1900Phrase(){
+        PageObjectBook books = PageFactory.initElements(driver, PageObjectBook.class);
+        books.searchBooks("1900");
+        assertAll(
+                () -> assertEquals(2, books.sizeOfTable()),
+                () -> assertTrue(books.getTableContent().get(0).getText().contains("1900")),
+                () -> assertTrue(books.getTableContent().get(1).getText().contains("1900"))
+        );
+    }
+    
+}
---
+++ src/test/java/PageObject/PageObjectBook.java
@@ -0,0 +1,122 @@
+package PageObject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
+public class PageObjectBook {
+    public WebDriver driver;
+
+    @FindBy(name = "Title")
+    private WebElement title;
+
+    @FindBy(id = "Length")
+    private WebElement length;
+
+    @FindBy(id = "ReleaseDate")
+    private WebElement releaseDate;
+
+    @FindBy(name = "Price")
+    private WebElement price;
+
+    @FindBy(id = "AuthorId")
+    private WebElement authorID;
+
+    @FindBy(id = "BookId")
+    private WebElement formDelete;
+
+    @FindBy(className = "text-danger")
+    private WebElement validationError;
+
+    @FindBy(name = "SearchString")
+    private WebElement searchBar;
+
+
+    @FindBy(xpath = "//table[@class='table']/tbody/tr")
+    private List<WebElement> tableContent;
+
+
+    public PageObjectBook(WebDriver driver){
+        this.driver = driver;
+    }
+
+    public void toTable(){
+        driver.get("http://bookcatalog.azurewebsites.net/Books");
+    }
+
+    public void toCreate(){
+        driver.get("http://bookcatalog.azurewebsites.net/Books/Create");
+
+    }
+
+    public void createBooks(String title, String length, String date, String price, String authorID){
+        this.title.sendKeys(title);
+        this.length.sendKeys(length);
+        this.releaseDate.sendKeys(date);
+        this.price.sendKeys(price);
+        this.authorID.sendKeys(authorID);
+        this.authorID.submit();
+    }
+
+    public void searchBooks(String searchPhrase){
+        searchBar.sendKeys(searchPhrase);
+        searchBar.submit();
+    }
+
+    public String getBook(int id){
+        WebElement tr = driver.findElement(By.xpath("(//table[@class='table']/tbody/tr)[" + id + "]"));
+        return tr.getText();
+    }
+
+    public void editBook(int id, String title, String length, String date, String price, String authorID){
+        driver.get("http://bookcatalog.azurewebsites.net/Authors/Edit/" + id);
+        this.title.clear();
+        this.title.sendKeys(title);
+
+        this.length.clear();
+        this.length.sendKeys(length);
+
+        this.releaseDate.clear();
+        this.releaseDate.sendKeys(date);
+
+        this.price.clear();
+        this.price.sendKeys(price);
+
+        this.authorID.clear();
+        this.authorID.sendKeys(authorID);
+
+        this.authorID.submit();
+    }
+
+    public int sizeOfTable(){
+        return tableContent.size();
+    }
+
+    public List<WebElement> getTableContent(){
+        return tableContent;
+    }
+
+    public Boolean assertValidationError(String message){
+        return validationError.getText().contains(message);
+    }
+
+    public String assertAddedRecord() {
+        WebElement tr = driver.findElement(By.xpath("(//table[@class='table']/tbody/tr)[last()]"));
+        return tr.getText();
+    }
+
+    public String assertRecord(int id) {
+        WebElement tr = driver.findElement(By.xpath("(//table[@class='table']/tbody/tr)[" + id + "]"));
+        return tr.getText();
+    }
+
+    public void deleteLastItem(){
+        WebElement tr = driver.findElement(By.xpath("(//table[@class='table']/tbody/tr[last()]/td[last()]/a[last()])"));
+        driver.navigate().to(tr.getAttribute("href"));
+        formDelete.submit();
+    }
+}
+
